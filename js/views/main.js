af5e71915a0555388f0d34c3babee1fafc937056
Dustin Smith <thedustinsmith@gmail.com>
2014-06-23 08:05:52 -0500

    More experimenting with the api

--- js/views/main.js
+++ js/views/main.js
@@ -1,6 +1,59 @@
 var mainView = new (Backbone.View.extend({
 	events: {
+		'click #btn-search': 'search',
+		'click .song-listing a': 'addSong'
+	},
 
+	search: function(ev) {
+		var searchTerm = this.$searchText.val(),
+			searchType = this.$searchTypes.filter(":checked").val();
+
+		if (!searchTerm || !searchType) {
+			alert("Please fill otu search form");
+			return;
+		}
+
+		$.ajax({
+			url: app.apiRoot + 'search',
+			type: 'GET',
+			headers: app.viewstate.get("RequestHeader"),
+			data: {
+				q: searchTerm,
+				type: searchType
+			}
+		}).done(_.bind(this.renderSearchResults, this));
+	},
+
+	addSong: function (ev) {
+		var playlistID = '1BrVvW61DspS9XduxnBdRL',
+			songID = $(ev.target).data('trackid');
+
+		var data = JSON.stringify(['spotify:track:' + songID]);
+		$.ajax({
+			url: this.userApiRoot + 'playlists/' + playlistID + '/tracks',
+			type: 'POST',
+			headers: app.viewstate.get('RequestHeader'),
+			data: data
+		}).done(function (resp) {
+			console.log(resp);
+		});
+	},
+
+	renderSearchResults: function (results) {
+		console.log(results);
+		var resultsWrap = this.$searchListing;
+		function appendSection(title, template, items) {
+			resultsWrap.append("<h3>" + title + "</h3>");
+			resultsWrap.append(template(items));
+		}
+
+		resultsWrap.html("");
+		if (results.artists.items.length) {
+			appendSection('Artists', app.templates.artistTemplate, results.artists.items);
+		}
+		if (results.tracks.items.length) {
+			appendSection('Songs', app.templates.songTemplate, results.tracks.items);
+		}
 	},
 
 	getPlaylists: function () {
@@ -14,13 +67,9 @@ var mainView = new (Backbone.View.extend({
 			headers: header
 		}).done(function (resp) {
 			app.viewstate.set('Playlists', resp.items);
-			var pls = _.map(resp.items, function (i) {
-				return {
-					pname: i.name
-				};
-			});
-			console.log(pls,app.templates.playlistTemplate(pls));
-			self.playlistListing.html(app.templates.playlistTemplate(pls));
+			var pls = resp.items;
+			console.log(pls);
+			self.$playlistListing.html(app.templates.playlistTemplate(pls));
 		});
 	},
 
@@ -31,7 +80,11 @@ var mainView = new (Backbone.View.extend({
 
 	initialize: function () {
 
-		this.playlistListing = this.$("#playlist-listing");
+		this.$playlistListing = this.$("#playlist-listing");
+		this.$searchText = this.$("#search");
+		this.$searchTypes = this.$("[name='search-type']");
+		this.$searchListing = this.$("#search-results");
+
 		var self = this;
 		this.listenTo(app.viewstate, 'change:User', function(m, v) {
 			if(!v) return;
